7bf04e8de8bc88aaf328f64eb7f418007212078d
lemonsaltytoo <v-lvkdl@naver.com>
2024-04-13 13:47:39 +0900

    Created coin-converter
    - need to fill coin symbol to label

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import styles from "./App.module.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  let selectedCoin;
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const [value, setValue] = useState(0);
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+  function handleReset() {
+    setValue(0);
+  }
+  function handleUnitChange(e) {
+    selectedCoin = e.target.value;
+    // 화면 출력이 안됨
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.main_box}>
+      <h2>Coin Converter 🪙</h2>
+      <p></p>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          {" "}
+          <select
+            onChange={(e) => {
+              handleUnitChange(e);
+            }}
+          >
+            {coins.map((coin) => {
+              return (
+                <option key={coin.id} value={coin.symbol}>
+                  {coin.name} - {coin.symbol} : {coin.quotes.USD.price} USD
+                </option>
+              );
+            })}
+          </select>
+          <p></p>
+          <div>
+            <input
+              id="usd"
+              type="number"
+              value={value}
+              onChange={handleChange}
+            />
+            <label htmlFor="usd"> USD</label>
+            <span> = </span>
+            <input
+              id="unit"
+              type="number"
+              value={value * 0.000015}
+              onChange={handleChange}
+            />
+            <label htmlFor="unit">{selectedCoin}</label>
+            <p></p>
+            <button onClick={handleReset}>Reset</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
